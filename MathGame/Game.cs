f0f3b14c9c9af01df2bb8f2b704f0fe4781a5d13
echoblade100 <echoblade100@gmail.com>
2024-07-01 17:56:16 +0300

    add random game, pick the number of questions, game timer

--- MathGame/Game.cs
+++ MathGame/Game.cs
@@ -3,14 +3,18 @@ namespace MathGame;
 public class Game
 {
     private int[][][] _gameLevels;
-    private int _currentLevel;
+    private int _currentDifficultyLevel;
+    private int _currentNumberOfQuestions;
+    private DateTime? _gameTimer;
     private const int MAX_LEVEL_OF_DIFFUCULTY = 5;
     private readonly Random _rnd = new Random();
     private readonly List<Stat> _stats = new List<Stat>();
 
     public Game()
     {
-        this._currentLevel = 0;
+        this._currentDifficultyLevel = 0;
+        this._currentNumberOfQuestions = 10;
+        this._gameTimer = null;
     }
 
     public void Run()
@@ -38,6 +42,8 @@ public class Game
                           $"- | subtraction game{Environment.NewLine}" +
                           $"* | multiplication game{Environment.NewLine}" +
                           $"/ | division game{Environment.NewLine}" +
+                          $"r | random game{Environment.NewLine}" +
+                          $"p | pick the number of questions{Environment.NewLine}" +
                           $"d | change level of difficulty{Environment.NewLine}" +
                           $"s | display stats{Environment.NewLine}" +
                           $"m | display menu{Environment.NewLine}" +
@@ -68,6 +74,14 @@ public class Game
                 case '/':
                     Console.WriteLine($"Start division game!");
                     this.StartDivisionGame();
+                    break;
+                case 'r':
+                    Console.WriteLine($"Start random game!");
+                    this.StartRandomGame();
+                    break;
+                case 'p':
+                    this.DisplayCurrentNumberOfQuestions();
+                    this.ChangeNumberOfQuestions();
                     break;
                 case 'd':
                     this.DisplayCurrentLevelOfDifficulty();
@@ -105,8 +119,9 @@ public class Game
 
     private void StartAdditionGame()
     {
-        int[][] pairs = this.SelectRandomPairsForGame();
+        int[][] pairs = this.SelectRandomPairsForGame(this._currentNumberOfQuestions);
         this.StartSaveStats("Addition", pairs.Length);
+        this.ToggleCurrentGameTimer();
         for (int i = 0; i < pairs.Length; i++)
         {
             this.DisplayGameData(i + 1, pairs.Length, pairs[i][0], pairs[i][1], "+");
@@ -116,14 +131,16 @@ public class Game
             this.SaveCorrectAnswer(answerResult);
             this.DisplayReplyState(answerResult);
         }
+        this.ToggleCurrentGameTimer();
 
         this.DisplayEndGameMessage();
     }
     
     private void StartSubtractionGame()
     {
-        int[][] pairs = this.SelectRandomPairsForGame();
+        int[][] pairs = this.SelectRandomPairsForGame(this._currentNumberOfQuestions);
         this.StartSaveStats("Subtraction", pairs.Length);
+        this.ToggleCurrentGameTimer();
         for (int i = 0; i < pairs.Length; i++)
         {
             this.DisplayGameData(i + 1, pairs.Length, pairs[i][0] + pairs[i][1], pairs[i][1], "-");
@@ -133,14 +150,16 @@ public class Game
             this.SaveCorrectAnswer(answerResult);
             this.DisplayReplyState(answerResult);
         }
+        this.ToggleCurrentGameTimer();
 
         this.DisplayEndGameMessage();
     }
     
     private void StartMultiplicationGame()
     {
-        int[][] pairs = this.SelectRandomPairsForGame();
+        int[][] pairs = this.SelectRandomPairsForGame(this._currentNumberOfQuestions);
         this.StartSaveStats("Multiplication", pairs.Length);
+        this.ToggleCurrentGameTimer();
         for (int i = 0; i < pairs.Length; i++)
         {
             this.DisplayGameData(i + 1, pairs.Length, pairs[i][0], pairs[i][1], "*");
@@ -150,14 +169,16 @@ public class Game
             this.SaveCorrectAnswer(answerResult);
             this.DisplayReplyState(answerResult);
         }
+        this.ToggleCurrentGameTimer();
 
         this.DisplayEndGameMessage();
     }
     
     private void StartDivisionGame()
     {
-        int[][] pairs = this.SelectRandomPairsForGame();
+        int[][] pairs = this.SelectRandomPairsForGame(this._currentNumberOfQuestions);
         this.StartSaveStats("Division", pairs.Length);
+        this.ToggleCurrentGameTimer();
         for (int i = 0; i < pairs.Length; i++)
         {
             this.DisplayGameData(i + 1, pairs.Length, pairs[i][0] * pairs[i][1], pairs[i][1], "/");
@@ -167,13 +188,57 @@ public class Game
             this.SaveCorrectAnswer(answerResult);
             this.DisplayReplyState(answerResult);
         }
+        this.ToggleCurrentGameTimer();
+
+        this.DisplayEndGameMessage();
+    }
+
+    private void StartRandomGame()
+    {
+        int[][] pairs = this.SelectRandomPairsForGame(this._currentNumberOfQuestions);
+        this.StartSaveStats("Random", pairs.Length);
+        this.ToggleCurrentGameTimer();
+        for (int i = 0; i < pairs.Length; i++)
+        {
+            int gameType = this._rnd.Next(0,4);
+            int answer = -1;
+            bool answerResult = false;
+
+            switch (gameType)
+            {
+                case 0:
+                    this.DisplayGameData(i + 1, pairs.Length, pairs[i][0], pairs[i][1], "+");
+                    answer = this.InputNumberHandler();
+                    answerResult = answer == pairs[i][0] + pairs[i][1];
+                    break;
+                case 1:
+                    this.DisplayGameData(i + 1, pairs.Length, pairs[i][0] + pairs[i][1], pairs[i][1], "-");
+                    answer = this.InputNumberHandler();
+                    answerResult = answer == pairs[i][0];
+                    break;
+                case 2:
+                    this.DisplayGameData(i + 1, pairs.Length, pairs[i][0], pairs[i][1], "*");
+                    answer = this.InputNumberHandler();
+                    answerResult = answer == pairs[i][0] * pairs[i][1];
+                    break;
+                case 3:
+                    this.DisplayGameData(i + 1, pairs.Length, pairs[i][0] * pairs[i][1], pairs[i][1], "/");
+                    answer = this.InputNumberHandler();
+                    answerResult = answer == pairs[i][0];
+                    break;
+            }
+
+            this.SaveCorrectAnswer(answerResult);
+            this.DisplayReplyState(answerResult);
+        }
+        this.ToggleCurrentGameTimer();
 
         this.DisplayEndGameMessage();
     }
 
     private void StartSaveStats(string gameName, int totalQuestions)
     {
-        this._stats.Add(new Stat() { GameName = gameName, TotalQuestions = totalQuestions, CorrectAnswers = 0});
+        this._stats.Add(new Stat() { GameName = gameName, TotalQuestions = totalQuestions, CorrectAnswers = 0, DifficultyLevel = _currentDifficultyLevel});
     }
     
     private void SaveCorrectAnswer(bool answer)
@@ -181,6 +246,20 @@ public class Game
         if (answer)
         {
             this._stats[^1].CorrectAnswers++;
+        }
+    }
+
+    private void ToggleCurrentGameTimer()
+    {
+        if (this._gameTimer == null)
+        {
+            this._gameTimer = DateTime.Now;
+        }
+        else
+        {
+            DateTime now = DateTime.Now;
+            this._stats[^1].GameTime = now.Subtract((DateTime)this._gameTimer);
+            this._gameTimer = null;
         }
     }
 
@@ -193,21 +272,42 @@ public class Game
             level = InputNumberHandler() - 1;
         } while (level < 0 || level >= MAX_LEVEL_OF_DIFFUCULTY);
 
-        this._currentLevel = level;
+        this._currentDifficultyLevel = level;
         this.DisplayCurrentLevelOfDifficulty();
     }
     
     private void DisplayCurrentLevelOfDifficulty()
     {
-        Console.WriteLine($"Current level of difficulty: {this._currentLevel + 1}");
+        Console.WriteLine($"Current level of difficulty: {this._currentDifficultyLevel + 1}");
     }
 
     private void DisplayStats()
     {
         foreach (var stat in this._stats)
         {
-            Console.WriteLine($"Game: {stat.GameName}, answers: {stat.CorrectAnswers}/{stat.TotalQuestions}");
+            Console.WriteLine($"Game: {stat.GameName}, difficulty:{stat.DifficultyLevel + 1}, answers: {stat.CorrectAnswers}/{stat.TotalQuestions} | time spent: {stat.GameTime}");
         }
+    }
+
+
+    private void ChangeNumberOfQuestions()
+    {
+        int numberOfQuestions = 0;
+        int maxNumberOfQuestions = _gameLevels[this._currentDifficultyLevel].Length / 2;
+
+        do
+        {
+            Console.Write($"Enter number of questions 1-{maxNumberOfQuestions}: ");
+            numberOfQuestions = InputNumberHandler();
+        } while (numberOfQuestions < 0 || numberOfQuestions > maxNumberOfQuestions);
+
+        this._currentNumberOfQuestions = numberOfQuestions;
+        this.DisplayCurrentNumberOfQuestions();
+    }
+
+    private void DisplayCurrentNumberOfQuestions()
+    {
+        Console.WriteLine($"Current number of questions: {this._currentNumberOfQuestions}");
     }
 
     private int[][] SelectRandomPairsForGame(int pairsCount = 10)
@@ -217,11 +317,11 @@ public class Game
 
         for (int i = 0; i < pairsCount; i++)
         {
-            int rndIndex = this._rnd.Next(0, _gameLevels[_currentLevel].Length);
+            int rndIndex = this._rnd.Next(0, _gameLevels[_currentDifficultyLevel].Length);
             if (!randomIndexes.Contains(rndIndex))
             {
                 randomIndexes[i] = rndIndex;
-                randomPairs[i] = _gameLevels[_currentLevel][rndIndex];
+                randomPairs[i] = _gameLevels[_currentDifficultyLevel][rndIndex];
             }
             else
             {
--- MathGame/Stat.cs
+++ MathGame/Stat.cs
@@ -5,4 +5,6 @@ public class Stat
     public string GameName { get; set; }
     public int CorrectAnswers { get; set; }
     public int  TotalQuestions { get; set; }
+    public int DifficultyLevel { get; set; }
+    public TimeSpan GameTime { get; set; }
 }
