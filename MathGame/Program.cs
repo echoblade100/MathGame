22adf9bd994e61246f9294637fd56ba191048afc
echoblade100 <echoblade100@gmail.com>
2024-07-03 07:55:23 +0300

    todo

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -2,3 +2,6 @@
 
 Game game = new Game();
 game.Run();
+
+//todo: refactoring DRY (Don't Repeat Yourself) principle. You could use one method for all games, since a lot of the code is the same. 
+// this. isn't necessary
